ac1684a20fbcbce349804e24a661fadd6f1eaf58
DHANANJAYA K R <dhananjayakrd@gmail.com>
2021-05-13 10:26:20 +0530

    checkbox

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [form, setForm] = useState({
+    textarea: "",
+  });
+  const [check, setCheck] = useState({ checkbox: false });
+  const [radiostate, setradiostate] = useState({ radio: false });
+
+  function formChange(e) {
+    const { name, value } = e.target;
+
+    setForm((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+  }
+
+  function submitForm(e) {
+    setForm({
+      textarea: "",
+    });
+
+    setCheck(false);
+    setradiostate(false);
+
+    e.preventDefault();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form>
+      <textarea
+        name="textarea"
+        value={form.textarea}
+        onChange={formChange}
+        id=""
+        cols="30"
+        rows="10"
+      >
+        demo
+      </textarea>
+
+      <input
+        name="radio"
+        value={form.radio}
+        checked={radiostate}
+        onChange={() => setradiostate(!radiostate)}
+        type="radio"
+      />
+
+      <input
+        name="checkbox"
+        value={check.checkbox}
+        checked={check}
+        type="checkbox"
+        onChange={() => setCheck(!check)}
+      />
+
+      <button onClick={submitForm}>Submit</button>
+    </form>
   );
 }
 
